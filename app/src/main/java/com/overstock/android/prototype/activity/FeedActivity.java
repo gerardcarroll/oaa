586d13074e9e52dabbd5dcde66bde11cb33b77ef
Ian Towey <itowey@overstock.com>
2016-03-08 16:06:12 +0000

    continue btn mods

--- app/src/main/java/com/overstock/android/prototype/activity/FeedActivity.java
+++ app/src/main/java/com/overstock/android/prototype/activity/FeedActivity.java
@@ -1,8 +1,12 @@
 package com.overstock.android.prototype.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,24 +20,11 @@ import com.overstock.android.prototype.R;
  */
 public class FeedActivity extends AppCompatActivity {
 
-  @Bind(R.id.oap_toolbar)
-  Toolbar toolbar;
-
-  @Bind(R.id.tvToolbarMsg)
-  TextView toolBarText;
-
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_feed);
-    ButterKnife.bind(this);
-
-    // Instantiate Toolbar
-    setSupportActionBar(toolbar);
-    setTitle("");
-    toolBarText.setText(R.string.feedToolbarText);
-
     Toast.makeText(this, "You made it!!", Toast.LENGTH_SHORT).show();
   }
+
 }
