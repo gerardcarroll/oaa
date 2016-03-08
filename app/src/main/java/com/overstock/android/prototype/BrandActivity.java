dd145cc7d394247bcb90dfee84d75247c5589644
Lee Meehan <lmeehan@overstock.com>
2016-03-08 10:54:26 +0000

    fixed indexing problem and manifest problem

--- app/src/main/java/com/overstock/android/prototype/BrandActivity.java
+++ app/src/main/java/com/overstock/android/prototype/BrandActivity.java
@@ -1,13 +1,16 @@
 package com.overstock.android.prototype;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class BrandActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_brand);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_brand);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction().add(R.id.brand_activity, new BrandFragment()).commit();
     }
+  }
 }
