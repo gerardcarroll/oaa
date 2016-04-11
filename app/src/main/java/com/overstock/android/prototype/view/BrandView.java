91f69043d8fad7829fe2396d4bf44deb46e3c714
Ian Towey <itowey@overstock.com>
2016-04-11 09:45:22 +0100

    update to brand view

--- app/src/main/java/com/overstock/android/prototype/view/BrandView.java
+++ app/src/main/java/com/overstock/android/prototype/view/BrandView.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * @author LeeMeehan Created on 07-Mar-16.
  */
 public interface BrandView {
-  
+
   void addHorizontialRecyclerView(final int layoutResourceId,final ArrayList<Product> parcelables, final String displayText);
 
 }
