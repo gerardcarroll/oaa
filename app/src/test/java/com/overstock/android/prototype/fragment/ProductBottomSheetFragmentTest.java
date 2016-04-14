e8b54239b064d138d97f95d9e6c36e4d0676419f
Lee Meehan <lmeehan@overstock.com>
2016-04-14 16:20:16 +0100

    Updated unit tests for product sheet

--- app/src/test/java/com/overstock/android/prototype/fragment/ProductBottomSheetFragmentTest.java
+++ app/src/test/java/com/overstock/android/prototype/fragment/ProductBottomSheetFragmentTest.java
@@ -1,26 +1,36 @@
 package com.overstock.android.prototype.fragment;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.parceler.Parcels;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
-import org.robolectric.annotation.Resetter;
+import org.robolectric.shadows.ShadowToast;
 
 import android.os.Build;
 import android.os.Bundle;
 
 import com.overstock.android.prototype.BuildConfig;
+import com.overstock.android.prototype.component.ApplicationComponent;
+import com.overstock.android.prototype.main.OAppPrototypeApplication;
 import com.overstock.android.prototype.model.Options;
 import com.overstock.android.prototype.model.ProductDetail;
+import com.overstock.android.prototype.module.ApplicationModule;
+import com.overstock.android.prototype.presenter.ProductBottomSheetPresenter;
+
+import it.cosenonjaviste.daggermock.DaggerMockRule;
 
 /**
  * @author LeeMeehan.
@@ -29,6 +39,16 @@ import com.overstock.android.prototype.model.ProductDetail;
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricGradleTestRunner.class)
 public class ProductBottomSheetFragmentTest {
+
+  @Rule
+  public final DaggerMockRule<ApplicationComponent> mockRule = new DaggerMockRule<>(ApplicationComponent.class,
+      new ApplicationModule(new OAppPrototypeApplication()))
+          .set(new DaggerMockRule.ComponentSetter<ApplicationComponent>() {
+            @Override
+            public void setComponent(ApplicationComponent applicationComponent) {
+              ((OAppPrototypeApplication) RuntimeEnvironment.application).setComponent(applicationComponent);
+            }
+          });
 
   private static final Integer PRODUCT_DETAIL_ID = 1001;
 
@@ -40,13 +60,13 @@ public class ProductBottomSheetFragmentTest {
 
   private static final String IMAGE_MEDIUM = "TEST_IMAGE_MEDIUM";
 
-  private static final Float PRICE = new Float(22.50);
+  private static final Float PRICE = Float.valueOf("22.50");
 
-  private static final Float OPTION_PRICE = new Float(25.50);
+  private static final Float OPTION_PRICE = Float.valueOf("25.50");
 
   private static final Integer QUANTITY = 5;
 
-  private static final Integer OPTION_QUANTITY_ALLOWD = 7;
+  private static final Integer OPTION_QUANTITY_ALLOWED = 7;
 
   private static final Integer OPTION_QUANTITY_ON_HAND = 10;
 
@@ -56,14 +76,20 @@ public class ProductBottomSheetFragmentTest {
 
   private ProductBottomSheetFragment productBottomSheetFragment;
 
+  private ProductDetail productDetails;
+
+  private ArrayList<Options> options;
+
+  @Mock
+  private ProductBottomSheetPresenter productBottomSheetPresenter;
+
   @Before
   public void setUp() {
     productBottomSheetFragment = new ProductBottomSheetFragment();
-    final List<Options> options = new ArrayList<>();
-    final Options option = new Options(OPTION_ID, OPTION_DESCRIPTION, OPTION_QUANTITY_ON_HAND, OPTION_QUANTITY_ALLOWD,
+    options = new ArrayList<>();
+    final Options option = new Options(OPTION_ID, OPTION_DESCRIPTION, OPTION_QUANTITY_ON_HAND, OPTION_QUANTITY_ALLOWED,
         OPTION_PRICE);
     options.add(option);
-    final ProductDetail productDetails;
     productDetails = new ProductDetail(PRODUCT_DETAIL_ID, PRODUCT_NAME, IMAGE_LARGE, IMAGE_MEDIUM, PRICE, QUANTITY,
         DESCRIPTION, options);
     productBottomSheetFragment = new ProductBottomSheetFragment();
@@ -76,17 +102,28 @@ public class ProductBottomSheetFragmentTest {
   @Test
   public void testProductBottomSheetFragmentNotNull() {
     assertNotNull(productBottomSheetFragment);
+    verify(productBottomSheetPresenter).setView(productBottomSheetFragment);
+    verify(productBottomSheetPresenter).updateProductPage(productDetails);
   }
 
   @Test
-  public void testQuantity(){
+  public void testQuantity() {
     productBottomSheetFragment.addQuantity();
+    verify(productBottomSheetPresenter).addQuantity(any(Integer.class));
     productBottomSheetFragment.removeQuantity();
+    verify(productBottomSheetPresenter).removeQuantity(any(Integer.class));
   }
 
-  @Resetter
-  public void reset(){
-    Robolectric.reset();
+  @Test
+  public void testDisplayToast() {
+    final String testMessage = "HI THERE.";
+    productBottomSheetFragment.displayToast(testMessage);
+    assertEquals(testMessage, ShadowToast.getTextOfLatestToast());
+  }
+
+  @Test
+  public void updateSpinner() {
+    productBottomSheetFragment.updateSpinner(options);
   }
 
 }
