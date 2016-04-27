52d4c4edfde3d128f08fcca9d739710f9ba2f1f0
Gerard Carroll <gcarroll@overstock.com>
2016-04-27 17:11:08 +0100

    add unit tests

--- app/src/test/java/com/overstock/android/prototype/presenter/SignInWithEmailPresenterTest.java
+++ app/src/test/java/com/overstock/android/prototype/presenter/SignInWithEmailPresenterTest.java
@@ -1,43 +1,65 @@
 package com.overstock.android.prototype.presenter;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import android.os.Build;
 
-import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import com.overstock.android.prototype.BuildConfig;
+import com.overstock.android.prototype.presenter.impl.SignInWithEmailPresenterImpl;
+import com.overstock.android.prototype.service.ParseService;
+import com.overstock.android.prototype.view.SignInWithEmailView;
 
 /**
  * Created by gcarroll on 27/04/2016.
  */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP, application =  com.overstock.android.prototype.TestOAppPrototypeApplication.class)
+@RunWith(RobolectricGradleTestRunner.class)
 public class SignInWithEmailPresenterTest {
 
-    @Before
-    public void setUp() throws Exception {
+  private static final String USERNAME = "gcarroll@overstock.com";
 
-    }
+  private static final String PASSWORD = "1234";
 
-    @After
-    public void tearDown() throws Exception {
+  @Mock
+  private ParseService parseService;
 
-    }
+  @Mock
+  private SignInWithEmailView signInWithEmailView;
 
-    @Test
-    public void testSetView() throws Exception {
+  private SignInWithEmailPresenter signInWithEmailPresenter;
 
-    }
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    signInWithEmailPresenter = new SignInWithEmailPresenterImpl(parseService);
+    signInWithEmailPresenter.setView(signInWithEmailView);
+  }
 
-    @Test
-    public void testOnSignIn() throws Exception {
+  @Test
+  public void testValidateCredentials_GOOD() throws Exception {
+    signInWithEmailPresenter.validateCredentials(USERNAME, PASSWORD);
+    verify(parseService).loginParseUser(USERNAME, PASSWORD, signInWithEmailView);
+  }
 
-    }
+  @Test
+  public void testValidateCredentials_NO_USERNAME() throws Exception {
+    signInWithEmailPresenter.validateCredentials("", PASSWORD);
+    verify(signInWithEmailView).displayToast(anyString());
+  }
 
-    @Test
-    public void testOnDestroy() throws Exception {
-
-    }
-
-    @Test
-    public void testValidateCredentials() throws Exception {
-
-    }
+  @Test
+  public void testValidateCredentials_NO_PASSWORD() throws Exception {
+    signInWithEmailPresenter.validateCredentials(USERNAME, "");
+    verify(signInWithEmailView).displayToast(anyString());
+  }
 }
