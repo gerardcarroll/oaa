56238b322a15a5a9e90f97188f4ea07bf73fb687
Lee Meehan <lmeehan@overstock.com>
2016-05-09 12:54:13 +0100

    Added small unit test

--- app/src/test/java/com/overstock/android/prototype/presenter/ProductBottomSheetPresenterTest.java
+++ app/src/test/java/com/overstock/android/prototype/presenter/ProductBottomSheetPresenterTest.java
@@ -4,18 +4,22 @@ import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
 
 import com.overstock.android.prototype.model.Options;
 import com.overstock.android.prototype.model.ProductDetail;
 import com.overstock.android.prototype.presenter.impl.ProductBottomSheetPresenterImpl;
+import com.overstock.android.prototype.service.CheckOutCoordinator;
 import com.overstock.android.prototype.service.CheckOutCoordinatorImpl;
 import com.overstock.android.prototype.view.ProductBottomSheetView;
 
@@ -23,6 +27,7 @@ import com.overstock.android.prototype.view.ProductBottomSheetView;
  * @author LeeMeehan
  * @since Created on 14-Apr-16.
  */
+@RunWith(RobolectricTestRunner.class)
 public class ProductBottomSheetPresenterTest {
 
   private static final Integer PRODUCT_DETAIL_ID = 1001;
@@ -55,15 +60,20 @@ public class ProductBottomSheetPresenterTest {
 
   private static final Float CURRENT_PRICE = Float.valueOf("2.00");
 
+  private static final String PARSE_USER_CART_ID = "11111";
+
   private ProductBottomSheetPresenter productBottomSheetPresenter;
 
   @Mock
   ProductBottomSheetView productBottomSheetView;
 
+  @Mock
+  CheckOutCoordinator checkOutCoordinator;
+
   @Before
   public void setUp() {
     MockitoAnnotations.initMocks(this);
-    productBottomSheetPresenter = new ProductBottomSheetPresenterImpl(new CheckOutCoordinatorImpl());
+    productBottomSheetPresenter = new ProductBottomSheetPresenterImpl(checkOutCoordinator);
     productBottomSheetPresenter.setView(productBottomSheetView);
     productBottomSheetPresenter.setCurrentPrice(CURRENT_PRICE);
   }
@@ -158,6 +168,12 @@ public class ProductBottomSheetPresenterTest {
     productBottomSheetPresenter.applyDiscount(totalPrice, discount);
     verify(productBottomSheetView).updateRewardsAmount(remainingReward_amount);
     verify(productBottomSheetView).updateFinalPrice(price_output);
+  }
+
+  @Test
+  public void testInitiateCheckout(){
+    when(checkOutCoordinator.getCartIdForParseUser()).thenReturn(PARSE_USER_CART_ID);
+    productBottomSheetPresenter.initiateCheckout();
   }
 
   @Test(expected = NullPointerException.class)
